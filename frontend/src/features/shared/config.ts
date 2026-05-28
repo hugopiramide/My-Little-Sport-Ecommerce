02c00a98b5c1519e0af60fad29a61f8398810c39
hugopiramide <hugocastillo.deus@gmail.com>
2026-05-28 08:34:03 +0200

    feat(front): Add public credentials for paypal and stripe

---
+++ frontend/src/features/shared/config.ts
@@ -0,0 +1,4 @@
+export const PAYMENT_CONFIG = {
+  stripePublishableKey: 'pk_test_51TPKijRyTMQr2Xv16HFf9kC6YbnQvT2Gxraxl9ooOeNJaH59dNk5HA8pgxB2AKcRUvImXS9EqPRHYm1ARWqf78c000Fhh2tMBP',
+  paypalClientId: 'AXGYsXEhEeoOaRdzuPYpLb4KSnpKpJdNjvWYnq8fvVSflNH-7Jp6AKhclqf0mzqiamro7g-x5-UWQGkA',
+};
